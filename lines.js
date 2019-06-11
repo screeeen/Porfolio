bd7d6f97d8ef45ef4d9126d42679854d55445406
screeeen <miguel@screenimplosion.com>
2019-06-11 19:02:43 +0200

    structure

--- lines.js
+++ lines.js
@@ -1,44 +1,24 @@
-
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
+var index = 1;
 
-var index = 1;                     //  set your counter to 1
+function myLoop() {
 
-function myLoop () {           //  create a loop function
-   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+  setTimeout(function () {
     let line = document.createElement("div");
     line.className = "line";
     line.style.height = "2px";
-    // line.style.top = "4" + index + "px";
     line.style.background = "#00000055";
-    // line.style.animationDelay = "." + index + "s";
-    // line.style.animationDuration = index + "s";
-    let elementHeight = document.getElementById("game").offsetHeight;
-  
-    document.getElementById("game").appendChild(line);        //  your code here
-    index+=2;                     //  increment the counter
-      if (index < elementHeight) {            //  if the counter < 10, call the loop function
-         myLoop();             //  ..  again which will trigger another 
-      }                        //  ..  setTimeout()
-   }, 10)
+
+    let elementHeight = document.getElementById("content").offsetHeight;
+
+    document.getElementById("content").appendChild(line);
+    index += 2;
+    if (index < elementHeight) {
+      myLoop();
+    }
+  }, 10)
 }
 
-myLoop();    
-
-
-// for (let index = 0; index < h; index += 4) {
-  
-//   let line = document.createElement("div");
-//   line.className = "line";
-//   line.style.top = index + "px";
-//   // line.style.height = "."index+"px";
-//   // line.style.background = "blue";
-//   // line.style.animationDelay = "." + index + "s";
-//   line.style.animationDuration = index + "s";
-//   document.getElementById("content").appendChild(line);
-// }
-
-
-// var id = setInterval(frame, 5);
-// clearInterval(id);
+myLoop();
