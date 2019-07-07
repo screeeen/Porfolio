2bbde9c3e88d1786cfcc86baafc50711077ec697
screeeen <miguel@screenimplosion.com>
2019-07-07 16:31:21 +0200

    progress bar

--- progress.js
+++ progress.js
@@ -1,16 +1,19 @@
-var bar = document.getElementsByClassName("progress-bar");
-// var valueNow = document.getElementsByClassName("progress-bar")[0].getAttribute("aria-valuenow");
-console.log("hola");
+var bar = document.getElementsByClassName("progress-bar")[0];
+var theBody = document.querySelector("body");
+var total = theBody.scrollTop; // window.innerHeight;
+let last_known_scroll_position = 0;
 
+var progress = theBody.scrollTop;
+bar.setAttribute("aria-valuemax", "window.innerHeight");
+bar.setAttribute("aria-valuenow", "window.innerHeight");
 let progressAmount;
-changeBar = () => {
-  progressAmount +=1;
-  console.log("value",progressAmount);
+window.addEventListener('scroll', function (e) {
+  progressAmount = window.scrollY / total;
+  console.log("hola", window.innerHeight, progressAmount);
+  bar.setAttribute("Style", "width:" + progressAmount + "%");
 
-  document.getElementsByClassName("progress-bar")[0].setAttribute("aria-valuenow",progressAmount);
+  document.getElementsByClassName("progress-bar")[0].setAttribute("aria-valuenow", progressAmount);
 }
-
-
-
+);
 
 
