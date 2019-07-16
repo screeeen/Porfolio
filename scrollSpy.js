2eba3d29d0e34f11156fe8bfc2d51b102d0b68e8
screeeen <miguel@screenimplosion.com>
2019-07-16 21:35:02 +0200

    mailing redirect

---
+++ scrollSpy.js
@@ -0,0 +1,2 @@
+console.log("scrollspy", document.getElementsByClassName("scrollspy-example")[0]);
+document.getElementsByClassName("scrollspy-example")[0].scrollspy({ target: '#menu-options', offset: -75 });
