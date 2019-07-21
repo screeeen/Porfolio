644f857919b5d742c08abc428b2c8c3e0c88f271
screeeen <miguel@screenimplosion.com>
2019-07-22 00:44:35 +0200

    updated endpoint

--- form.js
+++ form.js
@@ -2,7 +2,6 @@
 
 // Adds an event listener to our form. When the form is submitted, it will send data to our Lambda function, which in turn, will send us an email.
 document.getElementById('serverless-contact-form').addEventListener('submit', sendDataToLambda(e));
-console.log(document.getElementById('serverless-contact-form'));
 
 // Now for the good stuff. This is the function that will send our data to AWS.
 function sendDataToLambda(e) {
@@ -16,7 +15,10 @@ function sendDataToLambda(e) {
   // This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
   var endpoint = 'https://t5s6zttfe0.execute-api.us-east-1.amazonaws.com/prod/contactme'; 
   
-  document.getElementById('serverless-contact-form').reset();
+  console.log(document.getElementById('serverless-contact-form'));
+
+
+  // document.getElementById('serverless-contact-form').reset();
 
   // Remember those form values we just grabbed? We're going to put them into an object here.
   var body = {
@@ -26,16 +28,16 @@ function sendDataToLambda(e) {
     // subject: formSubject,
     message: formMessage
   }
-  // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
-  var lambdaRequest = new Request(endpoint, {
-    method: 'GET',
-    // Quick note: 'no-cors' mode is for development on localhost only!
-    mode: 'no-cors',
-    body: JSON.stringify(body)
-  });
-  // Call the Fetch API to make our request
-  fetch(lambdaRequest)
-    // This is where you can handle errors. This is just an example, so we won't cover that.
-    .then(response => console.log(response))
-    .catch(err => console.log(err));
+  // // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
+  // var lambdaRequest = new Request(endpoint, {
+  //   method: 'POST',
+  //   // Quick note: 'no-cors' mode is for development on localhost only!
+  //   mode: 'no-cors',
+  //   body: JSON.stringify(body)
+  // });
+  // // Call the Fetch API to make our request
+  // fetch(lambdaRequest)
+  //   // This is where you can handle errors. This is just an example, so we won't cover that.
+  //   .then(response => console.log(response))
+  //   .catch(err => console.log(err));
 }
