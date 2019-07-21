c1e9527bc5af0e0987ba4f79778eb48ac394622a
screeeen <miguel@screenimplosion.com>
2019-07-22 00:56:25 +0200

    updated endpoint

--- form.js
+++ form.js
@@ -29,16 +29,16 @@ function sendDataToLambda(e) {
     // subject: formSubject,
     message: formMessage
   }
-  // // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
-  // var lambdaRequest = new Request(endpoint, {
-  //   method: 'POST',
-  //   // Quick note: 'no-cors' mode is for development on localhost only!
-  //   mode: 'no-cors',
-  //   body: JSON.stringify(body)
-  // });
-  // // Call the Fetch API to make our request
-  // fetch(lambdaRequest)
-  //   // This is where you can handle errors. This is just an example, so we won't cover that.
-  //   .then(response => console.log(response))
-  //   .catch(err => console.log(err));
+  // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
+  var lambdaRequest = new Request(endpoint, {
+    method: 'POST',
+    // Quick note: 'no-cors' mode is for development on localhost only!
+    mode: 'no-cors',
+    body: JSON.stringify(body)
+  });
+  // Call the Fetch API to make our request
+  fetch(lambdaRequest)
+    // This is where you can handle errors. This is just an example, so we won't cover that.
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
 }
