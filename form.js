32aa3518170878d787c1c9f4dee505262cb1d43f
screeeen <miguel@screenimplosion.com>
2019-07-22 00:42:41 +0200

    updated endpoint

--- form.js
+++ form.js
@@ -28,7 +28,7 @@ function sendDataToLambda(e) {
   }
   // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
   var lambdaRequest = new Request(endpoint, {
-    method: 'POST',
+    method: 'GET',
     // Quick note: 'no-cors' mode is for development on localhost only!
     mode: 'no-cors',
     body: JSON.stringify(body)
