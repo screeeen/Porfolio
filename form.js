bd45a401600a91646862e0497161f95ed7147f22
screeeen <miguel@screenimplosion.com>
2019-07-22 15:07:11 +0200

    transition feedback after submit

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
 'use strict'
+console.log('hola');
 
 // Adds an event listener to our form. When the form is submitted, it will send data to our Lambda function, which in turn, will send us an email.
 document.getElementById('serverless-contact-form').addEventListener('submit', sendDataToLambda);
@@ -10,19 +11,19 @@ function sendDataToLambda(e) {
   var formName = document.querySelector('#name').value;
   var formCompany = document.querySelector('#company').value;
   var formEmail = document.querySelector('#email').value;
-  // var formSubject = document.querySelector('.form-subject').value;
+  var formBudget = document.querySelector('#budget').value;
   var formMessage = document.querySelector('#message').value;
   // This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
   var endpoint = 'https://t5s6zttfe0.execute-api.us-east-1.amazonaws.com/prod/contactme'; 
   
-  document.getElementById('serverless-contact-form').reset();
+  // document.getElementById('serverless-contact-form').reset();
 
   // Remember those form values we just grabbed? We're going to put them into an object here.
   var body = {
     name: formName,
     company: formCompany,
     email: formEmail,
-    // subject: formSubject,
+    budget: formBudget,
     message: formMessage
   }
   // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
@@ -39,5 +40,9 @@ function sendDataToLambda(e) {
     .then(response => console.log(response))
     .catch(err => console.log(err));
 
-    
+  document.getElementById('serverless-contact-form').setAttribute("hidden",true);
+  console.log(document.getElementById('serverless-contact-form'));
+  document.getElementById('thank-you').removeAttribute("hidden");
+
+
 }
