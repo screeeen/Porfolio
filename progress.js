21063b7ccb6cbabcb1a875660a0292e12c0e292d
screeeen <miguel@screenimplosion.com>
2019-08-29 15:25:00 +0200

    vr drafts in

--- progress.js
+++ progress.js
@@ -13,14 +13,32 @@ var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, h
 // var menu = document.getElementsByClassName("menu")[0];
 // var sticky = menu.offsetTop - (menu.offsetHeight);
 
+// window.addEventListener('onprogress', (event) => {
+//   if (pe.lengthComputable) {
+//     // document.body.style.opacity = pe.total;
+//     // document.body.style.opacity = pe.loaded;
+//     // document.body.style.opacity = pe;
+//     console.log(pe);
+//   }
+//   });
+
+
+// window.addEventListener('progress', (event) => {
+//   console.log('DOM fully loaded and parsed', document.body);
+//   document.body.style.opacity = ".1";
+// });
+
 window.addEventListener('scroll', function (e) {
   progressAmount = Math.round((window.scrollY) * 100 / (document.documentElement.scrollHeight - window.innerHeight));
   bar.setAttribute("Style", "width:" + progressAmount + "%");
   document.getElementsByClassName("progress-bar")[0].setAttribute("aria-valuenow", progressAmount);
-  
+
+
+
+
   // if (window.pageYOffset >= sticky) {
   //   console.log(window.pageYOffset, sticky);
-    
+
   //   menu.classList.add("sticky-top")
   // } else {
   //   menu.classList.remove("sticky-top");
