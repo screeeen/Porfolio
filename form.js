362b1fefde3f95758c205f65edb5c322a7797a9f
screeeen <miguel@screenimplosion.com>
2019-11-29 00:47:40 +0100

    arreglar el form y retocar

--- form.js
+++ form.js
@@ -1,26 +1,26 @@
 'use strict'
 
-var burguerButton = document.getElementById('burguer');
-var burguerIcon = document.querySelector('.burguer-icon');
-var budget= document.getElementById('budget')
+// var burguerButton = document.getElementById('burguer');
+// var burguerIcon = document.querySelector('.burguer-icon');
+// var budget= document.getElementById('budget')
 var thankYou = document.querySelector('.thank-you')
 var contactForm = document.getElementById('serverless-contact-form')
 // var backToForm = document.getElementsByClassName('back-to-form')[0]
-var menuAndForm = document.getElementById('utilityColumn');
+// var menuAndForm = document.getElementById('utilityColumn');
 
 // Adds an event listener to our form. When the form is submitted, it will send data to our Lambda function, which in turn, will send us an email.
 contactForm.addEventListener('submit', sendDataToLambda);
 // backToForm.addEventListener('click',showForm);
-burguerButton.addEventListener('click',toggleButton);
+// burguerButton.addEventListener('click',toggleButton);
 
 // Now for the good stuff. This is the function that will send our data to AWS.
 function sendDataToLambda(e) {
   e.preventDefault();
   // Gets the values of each field in our form. This is the data we'll send to our Lambda function.
   var formName = document.querySelector('#name').value;
-  var formCompany = document.querySelector('#company').value;
+  // var formCompany = document.querySelector('#company').value;
   var formEmail = document.querySelector('#email').value;
-  var formBudget = document.querySelector('#budget').value;
+  // var formBudget = document.querySelector('#budget').value;
   var formMessage = document.querySelector('#message').value;
   // This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
   var endpoint = 'https://t5s6zttfe0.execute-api.us-east-1.amazonaws.com/prod/contactme';
@@ -30,9 +30,9 @@ function sendDataToLambda(e) {
   // Remember those form values we just grabbed? We're going to put them into an object here.
   var body = {
     name: formName,
-    company: formCompany,
+    // company: formCompany,
     email: formEmail,
-    budget: formBudget,
+    // budget: formBudget,
     message: formMessage
   }
   // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
@@ -55,7 +55,7 @@ function sendDataToLambda(e) {
 
 async function hideForm() {
   await document.getElementById('serverless-contact-form').classList.add("fade-out");
-  await document.getElementById('budget').setAttribute("disabled",true);
+  // await document.getElementById('budget').setAttribute("disabled",true);
   await document.getElementById('serverless-contact-form').setAttribute("disabled",true);
 }
 
@@ -76,11 +76,11 @@ async function hideFeedback() {
   
 }
 
-function toggleButton() {
-  menuAndForm.classList.toggle("sticky-top");
-  // burguerButton.classList.toggle("sticky-top");
-  burguerIcon.classList.toggle('fa-plus');  
-  burguerIcon.classList.toggle('fa-minus');
-}
+// function toggleButton() {
+//   menuAndForm.classList.toggle("sticky-top");
+//   // burguerButton.classList.toggle("sticky-top");
+//   burguerIcon.classList.toggle('fa-plus');  
+//   burguerIcon.classList.toggle('fa-minus');
+// }
 
 
