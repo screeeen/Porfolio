08211ba887341054a922400d20e690b920fb6066
screeeen <miguel@screenimplosion.com>
2020-03-05 19:31:20 +0100

    FUOC

---
+++ scripts/goto.js
@@ -0,0 +1,4 @@
+goto = (direction) => {
+  const URL = "file:///Users/miguelangelalvarez/portfolio/" + direction + '.html'
+  return URL;
+}
---
+++ scripts/load.js
@@ -0,0 +1,4 @@
+    js_Load = () => {
+      // document.body.style ="transition: visibility 125s ease"
+      document.body.style.visibility = 'visible';
+    } 
