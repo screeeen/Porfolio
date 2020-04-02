341a7217b273f5f5e771f3bcedf2f3bf41a1d9ae
screeeen <miguel@screenimplosion.com>
2020-04-02 12:29:49 +0200

    first commit -  test links

--- scripts/goto.js
+++ scripts/goto.js
@@ -1,7 +1,4 @@
 goto = (direction) => {
-  console.log("hostname", window.location.hostname);
   const URL = "/Porfolio/" + direction + ".html";
-  console.log("url formatted", URL);
-
   return URL;
 };
